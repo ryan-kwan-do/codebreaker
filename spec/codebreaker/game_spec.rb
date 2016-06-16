8c134381698aabed5c3b147acf3af22067b709c1
Ryan Armstrong <hello@ryanarmstrong.me>
2016-06-16 13:43:14 -0500

    added 3 passing examples

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,21 +2,45 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do 
+      	let(:s_output) { double('s_output').as_null_object }
+      	let(:game) { Game.new(s_output) }
+     
     describe '#start' do
-      before(:each) do
-      	@output = double('output').as_null_object
-      	@game = Game.new(@output)
-      end
-
       it "sends a welcome message" do
-      	@output.should_receive(:puts).with('Welcome to Codebreaker!')
-      	@game.start('1234')
+      	s_output.should_receive(:puts).with('Welcome to Codebreaker!')
+      	game.start('1234')
       end
 
       it "prompts for the first guess" do
-      	@output.should_receive(:puts).with('Enter guess:')
-      	@game.start('1234')
+      	s_output.should_receive(:puts).with('Enter guess:')
+      	game.start('1234')
       end
+    end
+
+    describe "#guess" do 
+      context "with no matches" do
+      	it "sends a mark with ''" do
+      	  game.start('1234')
+      	  s_output.should_receive(:puts).with('')
+      	  game.guess('5555')
+      	end
+      end
+
+      context "with one number match" do
+      	it "send a mark with '-'" do
+      	  game.start('1234')
+      	  s_output.should_receive(:puts).with('-')
+      	  game.guess('2555')
+      	end
+	  end
+
+	  context "with 1 exact match" do
+		it "sends a mark with '+'" do
+		  game.start('1234')
+		  s_output.should_receive(:puts).with('+')
+		  game.guess('1555')
+		end
+	  end
     end
   end
 end
